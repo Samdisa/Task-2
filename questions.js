42cc9122496d2aee6f8c391ee583ddc8c5308865
Samdisa <104253999+Samdisa@users.noreply.github.com>
2023-07-30 21:34:21 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which heading tag has highest priority?",
+    answer: "h1",
+    options: [
+      "h3",
+      "h1",
+      "h2",
+      "h6"
+    ]
+  },
+    {
+    numb: 2,
+    question: "To insert a Javascript into an HTML page, which tag is used?",
+    answer: "script",
+    options: [
+      "script='java'",
+      "javascript",
+      "script",
+      "link"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the extension of javascript?",
+    answer: ".js",
+    options: [
+      ".java",
+      ".json",
+      ".javascript",
+      ".js"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Javascript is an___________language?",
+    answer: "bject-oriented",
+    options: [
+      "object-oriented",
+      "object-based",
+      "procedural",
+      "none of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not a Javascript framework?",
+    answer: "cassandra",
+    options: [
+      "node",
+      "vue",
+      "react",
+      "cassandra"
+    ]
+  },
+ 
+
+];
